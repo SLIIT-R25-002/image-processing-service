4f7684cb87af778aa1dc1fac674c74b561b8f5d7
Sachinthaka Ayeshmantha <it21219320@my.sliit.lk>
2025-10-27 15:59:58 +0530

    Update download.py

--- download.py
+++ download.py
@@ -1,2 +1,8 @@
+# download.py
+import os
 import clip
-clip.load("ViT-B/32")
+
+os.makedirs("weights", exist_ok=True)
+# This will download ViT-B/32 to ./weights/ViT-B-32.pt if not already present
+clip.load("ViT-B/32", device="cpu", download_root="weights")
+print("Downloaded CLIP weights to ./weights")
